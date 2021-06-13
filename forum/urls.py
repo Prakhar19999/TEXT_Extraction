70a428999b61e2d1b56385bbf1a619e77e253225
Prakhar19999 <prakhar.srivastava19999@gmail.com>
2021-06-13 22:54:09 +0530

    Extract text from pdf and load into excel

--- forum/urls.py
+++ forum/urls.py
@@ -4,4 +4,5 @@ from .views import *
 
 urlpatterns = [
     path('',pdf_to_text,name='home_page'),
+    path(r'^export/xls/$', export_users_xls, name='export_users_xls'),
 ]
--- forum/views.py
+++ forum/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import *
 from .forms import *
 import xlsxwriter
+import xlwt
 import PyPDF2
 import re
 
@@ -25,6 +26,34 @@ def pdf_to_text(request):
                 email_list.append(e)
             for m in mobile_number:
                 mobile_list.append(m)
+            
+
+            response = HttpResponse(content_type='application/ms-excel')
+            response['Content-Disposition'] = 'attachment; filename="detail.xls"'
+            wb = xlwt.Workbook(encoding='utf-8')
+            ws = wb.add_sheet('Users')
+            
+            row_num = 0
+            font_style = xlwt.XFStyle()
+            font_style.font.bold = True
+            columns = ['email', 'contact number']
+            for col_num in range(len(columns)):
+                ws.write(row_num, col_num, columns[col_num], font_style)
+
+            font_style = xlwt.XFStyle()
+
+            for email in email_list:
+                row_num=row_num+1
+                ws.write(row_num,0,email,font_style)
+            row_num=0
+            for contact in mobile_list:
+                row_num=row_num+1
+                ws.write(row_num,1,contact,font_style)
+            wb.save(response)
+            return response
+            
+
+
     form=DocumentForm()
     context={
         'form':form,
@@ -34,25 +63,28 @@ def pdf_to_text(request):
     }
     return render(request,'forum/home.html',context)
 
-def excel_sheet(request):
-    if(request.method=="GET"):
-        response = HttpResponse(content_type='application/ms-excel')
-        response['Content-Disposition'] = 'attachment; filename="ThePythonDjango.xls"'
-        wb = xlwt.Workbook(encoding='utf-8')
-        ws = wb.add_sheet("sheet1")
-        row_num = 0
-        font_style = xlwt.XFStyle()
-        font_style.font.bold = True
-        columns = ['Column 1', 'Column 2', 'Column 3', 'Column 4', ]
-        for col_num in range(len(columns)):
-            ws.write(row_num, col_num, columns[col_num], font_style)
-        font_style = xlwt.XFStyle()
-        data = get_data()
-        for my_row in data:
-            row_num = row_num + 1
-            ws.write(row_num, 0, my_row.name, font_style)
-            ws.write(row_num, 1, my_row.start_date_time, font_style)
-            ws.write(row_num, 2, my_row.end_date_time, font_style)
-            ws.write(row_num, 3, my_row.notes, font_style)  
-        wb.save(response)
-        return response
+
+def export_users_xls(request):
+    response = HttpResponse(content_type='application/ms-excel')
+    response['Content-Disposition'] = 'attachment; filename="detail.xls"'
+    wb = xlwt.Workbook(encoding='utf-8')
+    ws = wb.add_sheet('Users')
+    # Sheet header, first row
+    row_num = 0
+    font_style = xlwt.XFStyle()
+    font_style.font.bold = True
+    columns = ['email', 'contact number']
+    for col_num in range(len(columns)):
+        ws.write(row_num, col_num, columns[col_num], font_style)
+
+    # Sheet body, remaining rows
+    font_style = xlwt.XFStyle()
+
+    rows = Document.objects.all().values_list('description', 'uploaded_at')
+    for row in rows:
+        row_num += 1
+        for col_num in range(len(row)):
+            ws.write(row_num, col_num, row[col_num], font_style)
+
+    wb.save(response)
+    return response
